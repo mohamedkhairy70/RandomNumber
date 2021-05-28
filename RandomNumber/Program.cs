265b789a2b23be72aefe8ecff2308127b382f8fd
mohamed khairy <mohamedkhairy70@outlook.com>
2021-05-28 18:38:34 +0200

    Add project files.

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Hello World!  {GenerateInvoiceNo()} :  {GenerateBarcodeNo()}");
+            }
+           
+            Console.ReadKey();
+        }
+        public static string GenerateInvoiceNo()
+        {
+            while (true)
+            {
+                var _numbers = "0123456789asdfghqwertyuio";
+                var random = new Random();
+
+                var builder = new StringBuilder(14);
+                var numbers = "";
+                for (var i = 0; i < 7; i++) builder.Append(_numbers[random.Next(0, _numbers.Length)]);
+                numbers = builder.ToString();
+
+                if (numbers != null)
+                {
+                    return numbers;
+                }
+            }
+        }
+
+        public static string GenerateBarcodeNo()
+        {
+            while (true)
+            {
+                var _numbers = "0123456789asdfghqwertyuio";
+                var random = new Random();
+
+                var builder = new StringBuilder(14);
+                var numbers = "";
+                for (var i = 0; i < 7; i++) builder.Append(_numbers[random.Next(0, _numbers.Length)]);
+                numbers = builder.ToString();
+
+                if (numbers != null)
+                {
+                    return numbers;
+                }
+            }
+        }
+    }
+}
